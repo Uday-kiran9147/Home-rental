908c5994e2fa4784b53b6f3b211b91b77c76e704
Uday-kiran9147 <udaykiran9147@gmail.com>
2024-04-29 11:20:20 +0530

    Update scripts in package.json

--- AppServer/src/app.ts
+++ AppServer/src/app.ts
@@ -7,8 +7,9 @@ import bodyParser from "body-parser";
 import { bookingRoute } from "./Routers/bookings";
 
 
-
+const mongooseUrl = "mongodb+srv://udaykiran9147:zoGPC7VIL3pZStRI@cluster0.pap26w0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const PORT = 3000;
+
 const app = express();
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 async function initializeMongoDBConnection() {
     console.log("Connecting........");
     try {
-        await Mongoose.connect('mongodb://127.0.0.1:27017/HouseRentals', { retryWrites: true, w: 'majority' });
+        await Mongoose.connect(mongooseUrl, { retryWrites: true, w: 'majority' });
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB');
@@ -29,5 +30,5 @@ app.use(signUpRoute);
 app.use(loginRoute);
 app.use(propertyRoute);
 app.use(bookingRoute);
-app.listen(3000, () => console.log(`Server started on port http://localhost:${PORT}`));
+app.listen(3000, () => console.log(`Server started on port https://houserental.onrender.com`));
 
