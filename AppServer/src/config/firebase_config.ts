92bfd04449469cdfa57171f6394687fc051fbbc6
Uday-kiran9147 <udaykiran9147@gmail.com>
2024-04-29 11:47:32 +0530

    Add Firebase configuration files

---
+++ AppServer/src/config/firebase_config.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyD8ahpkYKrKiAIzfBBckXUXTG_TTlROcxY",
+  authDomain: "ninja-5cdbe.firebaseapp.com",
+  databaseURL: "https://ninja-5cdbe-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ninja-5cdbe",
+  storageBucket: "ninja-5cdbe.appspot.com",
+  messagingSenderId: "587179209077",
+  appId: "1:587179209077:web:fdb6c8dc73b932525e633c"
+};
+
