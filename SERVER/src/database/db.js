69f76f8b0b41b80d677035705136b8664ab30350
uday <udaykiran9147@gmail.com>
2024-05-10 13:02:59 +0530

    delete: .js files

--- SERVER/src/database/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-
-const uri = "mongodb+srv://uday:TestDataBase@testingcluster.hmcvuvv.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-},).then(() => console.log('Connected to database')).catch((error) => console.log(error))
